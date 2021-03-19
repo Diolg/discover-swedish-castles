72f59d2882daf357419f8e181c495003ef36db1d
Diolg <olga.diakoff@gmail.com>
2021-03-19 18:15:19 +0000

    Add content to info windows for map

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -19,17 +19,17 @@ function initMap()
         {
         coordinates: {lat: 59.2354, lng: 18.3051},
         image: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
-        info: '<img src="assets/images/tyreso-castle.jpg" alt="castle" class="card-img-top"><h6><strong>Tyreso castle</strong></h6><br><p>Marked with flag, because it is my favorite.</p><br><p>My favorite castle was built in 1630s.</p><br><p>Currently it is a museum with beautiful parks and lakes.<p>Here you find: <ul><li>Museum</li><li>Hotel</li><li>SPA</li><li>Restaurant</li><li>Spaces for hiking</li><li>Natural swimming areas</li></ul></p><a href="https://www.nordiskamuseet.se/en/castles-manors/tyreso-palace" target="_blank">Check full information</a>'
+        info: '<div class="info-window"><img class="info-window-image" src="assets/images/tyreso-castle.jpg" alt="castle"><h6><strong>Tyreso castle</strong></h6><p>Marked with flag, because it is my favorite.</p><p>Built in 1630s currently it is a museum with beautiful parks and lakes.<p>Here you find: <ul><li>Museum</li><li>Hotel</li><li>SPA</li><li>Restaurant</li><li>Spaces for hiking</li><li>Natural swimming areas</li></ul></p><a href="https://www.nordiskamuseet.se/en/castles-manors/tyreso-palace" target="_blank">Check full information</a></div>'
     },
 
     {
         coordinates: {lat: 59.4349, lng: 17.7872},
-        info: '<img src="assets/images/gorvalnslott-castle.jpg" alt="castle" class="card-img-top"><h6><strong>Gorvalnslott castle</strong></h6><br><p>Built in 1670s castle is a perfect place for romantic time or for recharging batteries in luxury.</p><br><p>Here you find: <ul><li>Hotel</li><li>SPA</li><li>Restaurant</li><li>Spaces for walking</li><li>Natural swimming areas</li><li>Fishing areas</li><li>Facilities for winter skiing</li></ul></p></h1><a href="https://www.gorvalnsslott.se/en/" target="_blank">Check full information</a>'
+        info: '<div class="info-window"><img class="info-window-image" src="assets/images/gorvalnslott-castle.jpg" alt="castle"><h6><strong>Gorvalnslott castle</strong></h6><p>Built in 1670s castle is a perfect place for romantic time or for recharging batteries in luxury.</p><p>Here you find: <ul><li>Hotel</li><li>SPA</li><li>Restaurant</li><li>Spaces for walking</li><li>Natural swimming areas</li><li>Fishing areas</li><li>Facilities for winter skiing</li></ul></p></h1><a href="https://www.gorvalnsslott.se/en/" target="_blank">Check full information</a></div>'
     },
 
     {
         coordinates:{lat: 59.0393, lng: 18.0142},
-        info: '<img src="assets/images/herringe-castle.jpg" alt="castle" class="card-img-top"><h6><strong>Herringe castle</strong></h6><br><p>A cosy castle in a 1657 property offering dining & a bowling alley, plus a garden with a pool.</p><p>Here you find: <ul><li>Hotel</li><li>SPA</li><li>Restaurant</li><li>Outside swimming pool</li><li>Spaces for walking</li><li>Natural swimming areas</li><li>Fishing areas</li><li>Riding school</li></ul></p></h1><a href="http://www.haringeslott.se/haringe-slott" target="_blank">Check full information</a>'
+        info: '<div class="info-window"><img class="info-window-image" src="assets/images/herringe-castle.jpg" alt="castle"><h6><strong>Herringe castle</strong></h6><p>A cosy castle in a 1657 property offering dining & a bowling alley, plus a garden with a pool.</p><p>Here you find: <ul><li>Hotel</li><li>SPA</li><li>Restaurant</li><li>Outside swimming pool</li><li>Spaces for walking</li><li>Natural swimming areas</li><li>Fishing areas</li><li>Riding school</li></ul></p></h1><a href="http://www.haringeslott.se/haringe-slott" target="_blank">Check full information</a></div>'
     }
 
     ];
