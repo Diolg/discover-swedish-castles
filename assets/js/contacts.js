13763c68d1b82b1aaf3aa509d74aef30a00c8475
Diolg <olga.diakoff@gmail.com>
2021-03-23 23:11:04 +0000

    Add new comments to code

--- assets/js/contacts.js
+++ assets/js/contacts.js
@@ -1,127 +1,30 @@
-
-//Set the EmailJS
-    
- (function() {
+//Initialize EmailJS   
+(function() {
     emailjs.init("user_ugvGbZOApBxJa8Ka2h7Vk");
 })();
 
-//Retrieve the data from form inputs
+//Retrieve the data from inputs with EmailJS
 function sendMail(contactForm) {
+    emailjs.send("service_jgqb1x9", "contact_form", {
+            "message": contactForm.message.value,
+            "user_name": contactForm.name.value,
+            "user_email": contactForm.email.value,
+        })
+        //Set function for success or error response
+        .then(
+            function(response) {
+                console.log("SUCCESS", response);
+                document.getElementById("success-response").style.display = "block";
+                //Set Time out for user to manage to read the success-message
+                setTimeout(() => document.getElementById("success-response").hidden = true, 6000);
+            },
+            function(error) {
+                console.log("FAILED", error);
+                document.getElementById("error-response").style.display = "block";
+            });
 
-    emailjs.send("service_jgqb1x9","contact_form", 
-    {
-        "message": contactForm.message.value,
-        "user_name": contactForm.name.value,
-        "user_email": contactForm.email.value,
-        
-    })
-    
-// Set function for success or error response
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-                       document.getElementById("success-response").style.display="block";
-
-//Set Time out for user to manage to read the success-message
-    setTimeout(() => document.getElementById("success-response").hidden = true, 6000);
-
-    },
-        function(error) {
-            console.log("FAILED", error);
-           document.getElementById("error-response").style.display="block";              
-});
-
-//Cleaning the form
-
-document.getElementById("contact-form").reset();
-
-return false;
-} 
-
-
-
-
-
-
-
-
-
-
-
-
-//Function to show the user the message has been submitted successfully
-/*let contactForm = document.getElementById("contact-form");
-contactForm.addEventListener("submit", handleSubmit);
-function handleSubmit(event) {
-event.preventDefault(); 
-
-let name = contactForm.elements["name"].value;
-let email = contactForm.elements["email"].value;
-
-let successHtml = `
-<p>Hi ${name}! Your message has been sent successfully! <br> I will respond to ${email} in 2 hours! <br> 
-Back to <a href="index.html">home page</a></p>
-`
-
-let responseDiv = document.getElementById("success-response");   
-responseDiv.innerHTML = successHtml;  
-responseDiv.style.display = "block";  
-responseDiv.style.color = "white"; 
-
-// Hide the success window after submission
-
-setTimeout(() => responseDiv.hidden = true, 6000)
-
-} 
-
-// Cleans the form's inputs after submission
-
-let form = document.getElementById("contact-form");
-form.addEventListener("submit", function handleSubmit (event) {
-    event.preventDefault()
-    contactForm.reset()
-})  */
-
-//Check form
-
-/*let form = document.getElementById("contact-form");
-function checkForm(el) {
-  let name = form.el.name.value;
-  let message = form.el.message.value;
-
-  let fail = "";   
-
-  If(name.length <= 1 || name.length > 50)
-    fail = "Please, enter correct name";
-    else if(message.length > 5)
-    fail = "Please, use no more than 200 characters!"
-
-     if(fail !="") {
-         alert("fail");
-
-         return false;
-     }*/
-
-
-
-
-/*let form = document.getElementById("contact-form");
-function validation() {
-  let name = form.el.name.value;
-  let message = form.el.message.value;
-  let error = document.getElementById("error");
-  let text;
-
-  error.style.padding = "10px";
-
-  if(name.length <= 1 || name.length > 50) {
-      text = "please enter valid name!" ;
-      error.innerHTML = text;
-      return false;
-  }
-
-}*/
-
-
-
+    //Cleaning form after submission
+    document.getElementById("contact-form").reset();
+    return false;
+}
 
--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,40 +1,64 @@
-//Initialized Google Map function
+//Initialize Google Map function
 function initMap() {
     var element = document.getElementById('map');
-
-    //Created an object from parameters
     var options = {
         zoom: 8,
         center: {
             lat: 59.2354,
             lng: 18.3051
         },
-
     };
 
-    //Created map locations array
-
+    //Create map locations array
     var myMap = new google.maps.Map(element, options);
-
     var markers = [{
             coordinates: {
                 lat: 59.2354,
                 lng: 18.3051
             },
             image: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
-            info: `<div class="info-window"><img class="info-window-image" src="assets/images/tyreso-castle.jpg" alt="castle"><h6><strong>Tyreso castle</strong></h6>
-            <p>Marked with flag, because it is my favorite.</p>
-            <p>Built in 1630s currently it is a museum with beautiful parks and lakes.
-            <p>Here you find: <ul><li>Museum</li><li>Hotel</li><li>SPA</li><li>Restaurant</li><li>Spaces for hiking</li><li>Natural swimming areas</li></ul></p>
-            <a href="https://www.nordiskamuseet.se/en/castles-manors/tyreso-palace" target="_blank">Check full information</a></div>`
+            info: `<div class="info-window">
+                        <img class="info-window-image" src="assets/images/tyreso-castle.jpg" alt="castle">
+                        <h6><strong>Tyreso castle</strong></h6>
+                        <p>Marked with flag, because it is my favorite.</p>
+                        <p>Built in 1630s currently it is a museum with beautiful parks and lakes.
+                        <p>Here you find:
+                            <ul>
+                                <li>Museum</li>
+                                <li>Hotel</li>
+                                <li>SPA</li>
+                                <li>Restaurant</li>
+                                <li>Spaces for hiking</li>
+                                <li>Natural swimming areas</li>
+                            </ul>
+                        </p>
+                        <a href="https://www.nordiskamuseet.se/en/castles-manors/tyreso-palace" target="_blank">
+                        Check full information</a>
+                    </div>`
         },
 
-        {
-            coordinates: {
+        {   coordinates: {
                 lat: 59.4349,
                 lng: 17.7872
             },
-            info: '<div class="info-window"><img class="info-window-image" src="assets/images/gorvalnslott-castle.jpg" alt="castle"><h6><strong>Gorvalnslott castle</strong></h6><p>Built in 1670s castle is a perfect place for romantic time or for recharging batteries in luxury.</p><p>Here you find: <ul><li>Hotel</li><li>SPA</li><li>Restaurant</li><li>Spaces for walking</li><li>Natural swimming areas</li><li>Fishing areas</li><li>Facilities for winter skiing</li></ul></p></h1><a href="https://www.gorvalnsslott.se/en/" target="_blank">Check full information</a></div>'
+            info: `<div class="info-window">
+                        <img class="info-window-image" src="assets/images/gorvalnslott-castle.jpg" alt="castle">
+                        <h6><strong>Gorvalnslott castle</strong></h6>
+                        <p>Built in 1670s castle is a perfect place for romantic time or for recharging batteries in luxury.</p>
+                        <p>Here you find: 
+                            <ul>
+                                <li>Hotel</li>
+                                <li>SPA</li>
+                                <li>Restaurant</li>
+                                <li>Spaces for walking</li>
+                                <li>Natural swimming areas</li>
+                                <li>Fishing areas</li>
+                                <li>Facilities for winter skiing</li>
+                            </ul>
+                        </p>
+                        <a href="https://www.gorvalnsslott.se/en/" target="_blank">
+                        Check full information</a>
+                    </div>`
         },
 
         {
@@ -42,21 +66,33 @@ function initMap() {
                 lat: 59.0393,
                 lng: 18.0142
             },
-            info: '<div class="info-window"><img class="info-window-image" src="assets/images/herringe-castle.jpg" alt="castle"><h6><strong>Herringe castle</strong></h6><p>A cosy castle in a 1657 property offering dining & a bowling alley, plus a garden with a pool.</p><p>Here you find: <ul><li>Hotel</li><li>SPA</li><li>Restaurant</li><li>Outside swimming pool</li><li>Spaces for walking</li><li>Natural swimming areas</li><li>Fishing areas</li><li>Riding school</li></ul></p></h1><a href="http://www.haringeslott.se/haringe-slott" target="_blank">Check full information</a></div>'
+            info: `<div class="info-window">
+                        <img class="info-window-image" src="assets/images/herringe-castle.jpg" alt="castle">
+                        <h6><strong>Herringe castle</strong></h6>
+                        <p>A cosy castle in a 1657 property offering dining & a bowling alley, plus a garden with a pool.</p>
+                        <p>Here you find: 
+                            <ul>
+                                <li>Hotel</li>
+                                <li>SPA</li>
+                                <li>Restaurant</li>
+                                <li>Outside swimming pool</li>
+                                <li>Spaces for walking</li>
+                                <li>Natural swimming areas</li>
+                                <li>Fishing areas</li>
+                                <li>Riding school</li>
+                            </ul>
+                        </p>
+                        <a href="http://www.haringeslott.se/haringe-slott" target="_blank">Check full information</a>
+                    </div>`
         }
 
     ];
 
-
     //Added markers of castles locations on the map
-
-
     for (var i = 0; i < markers.length; i++)
-
     {
         addMarker(markers[i]);
     }
-
 
     function addMarker(properties) {
         var marker = new google.maps.Marker({
@@ -70,15 +106,13 @@ function initMap() {
             marker.setIcon(properties.image);
         }
 
-        // Set infoWindiws
-
+        //Set infoWindows
         if (properties.info) {
             var InfoWindow = new google.maps.InfoWindow({
                 content: properties.info
             });
 
-            // Added click listener to markers
-
+        // Added click listener to markers
             marker.addListener("click", function() {
                 InfoWindow.open(myMap, marker);
             });
@@ -86,38 +120,4 @@ function initMap() {
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
